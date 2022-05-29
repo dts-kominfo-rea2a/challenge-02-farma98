4860f4ccdd9617251f12ac179593f7d7141b2050
Mahfud Prasetyo <37892922+farma98@users.noreply.github.com>
2022-05-29 19:19:13 +0700

    Menyelesaikan tantangan 02

--- index.js
+++ index.js
@@ -18,29 +18,44 @@ function lakukanLooping(arrPegawai) {
   let dataYangAkanDilooping = arrPegawai;
 
   /*
-    TODO 1: Buatlah sebuah variabel bernama "hasilLooping" 
+    TODO 1: Buatlah sebuah variabel bernama "hasilLooping"
       yang berisi gabungan nama depan dan belakang dari masing masing pegawai
 
       Contoh: ["Aisyah Nirmala", "Mansur Faisal", ...]
   */
-  let hasilLooping = null;
+  let hasilLooping = [];
+  dataYangAkanDilooping.forEach((data) => {
+    hasilLooping.push(`${data.namaDepan} ${data.namaBelakang}`);
+  });
 
   /*
     TODO 2: Buatlah sebuah variabel bernama "jumlahPria"
       yang berisi jumlah pria dari masing masing pegawai
   */
-  let jumlahPria = null;
+  let jumlahPria = 0;
+
+  dataYangAkanDilooping.forEach((data) => {
+    if (data.jenisKelamin === "M") {
+      jumlahPria += 1;
+    }
+  });
 
   /*
     TODO 3: Buatlah sebuah variabel bernama "jumlahWanita"
       yang berisi jumlah wanita dari masing masing pegawai
   */
-  let jumlahWanita = null;
+  let jumlahWanita = 0;
+
+  dataYangAkanDilooping.forEach((data) => {
+    if (data.jenisKelamin === "F") {
+      jumlahWanita += 1;
+    }
+  });
 
   /*
     TODO 4: Buatlah sebuah variabel bernama "komentar"
       yang akan mengomentari apakah lebih banyak Pria atau Wanita
-      
+
       Apabila Pria lebih banyak dari Wanita, maka komentar akan berisi:
         "Jumlah Pria lebih banyak dari Wanita"
       Apabila Wanita lebih banyak dari Pria, maka komentar akan berisi:
@@ -48,7 +63,15 @@ function lakukanLooping(arrPegawai) {
       Apabila imbang, maka komentar akan berisi:
         "Jumlah Pria dan Wanita berimbang"
   */
-  let komentar = null;
+  let komentar = "";
+
+  if (jumlahPria > jumlahWanita) {
+    komentar = "Jumlah Pria lebih banyak dari Wanita";
+  } else if (jumlahWanita > jumlahPria) {
+    komentar = "Jumlah Wanita lebih banyak dari Pria";
+  } else {
+    komentar = "Jumlah Pria dan Wanita berimbang";
+  }
 
   // ! JANGAN DIMODIFIKASI
   return {
